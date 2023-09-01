c9e3cf6c2793d6a3a219df23f28eab48eed367e8
안재홍 <63873742+JaeHong-Ahn@users.noreply.github.com>
2023-09-02 00:56:49 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,62 +1,39 @@
 def solution(tickets):
-
-    answer = []
-
-    visited = [False for _ in range(len(tickets))]
-
-    # airport 출발지 찾음
-    def find_start_airport(airport, visited):
+    #airport 출발지 찾음
+    def find_start_airport(airport) :
         start = []
-        for i in range(len(tickets)):
-            if airport == tickets[i][0] and visited[i] == False:
+        for i in range(len(tickets)) :
+            if airport == tickets[i][0] :
                 start.append(tickets[i])
         # 알파벳순 정렬
-        start = sorted(start, key=lambda x: x[1])
-        return start
-
-    def dfs(previous, tickets, visited):
-
-        #중복 값 체크
-        loca = []
+        start = sorted(start, key = lambda x : x[1])
+        return start      
+    
+    def dfs(previous, tickets) :
+        
         for i in range(len(tickets)) :
-            if tickets[i] == previous :
-                loca.append(i)
-
-        for i in range(len(loca)) :
-            if visited[loca[i]] == False :
-                visited[loca[i]] = True
-                a.append(loca[i])
-                answer.append(previous[1])
-                break
-
-        #탈출 조건
-        if len(answer) == (len(tickets) + 1) and len(list(set(visited))) == 1:
-            return answer
-
-        if find_start_airport(previous[1], visited) == []:
+            if tickets[i][0] == previous[0] and tickets[i][1] == previous[1] :
+                tickets[i][0] == "visited"
+                tickets[i][1] == "visited"
+        
+        if not find_start_airport(previous[1]) :
             return False
-        else:
-            next = find_start_airport(previous[1], visited)
-
-        for i in range(len(next)):
-
-            if dfs(next[i], tickets, visited) :
-                return answer
-
-            answer.pop()
-            visited[a[-1]] = False
-            a.remove(a[-1])
-
-
-    present = find_start_airport("ICN", visited)
-    a = []
+        else :
+            next = find_start_airport(previous[1])
+        answer.append(previous[1])
+        
+        if len(set(tickets)) == 1 :
+            return answer
+        for i in range(len(next)) :
+            dfs(next[i], tickets)
+    
     answer = ["ICN"]
-    for i in range(len(present)) :
-
-        if dfs(present[i], tickets, visited) == False :
-            answer = ["ICN"]
-            visited = [False for _ in range(len(tickets))]
-            continue
-        else : dfs(present[i], tickets, visited)
-
+        
+    present = find_start_airport("ICN")
+    
+    # for i in range(len(present)) :
+    #     dfs(present[i], tickets)
+    
+    print(present)
+    
     return answer
