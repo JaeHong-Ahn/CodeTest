74469d09b2f3e831ba4e7aed1c646fef68adc83c
안재홍 <63873742+JaeHong-Ahn@users.noreply.github.com>
2023-11-09 16:57:33 +0900

    [level 2] Title: 방문 길이, Time: 13.25 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,44 @@
+def solution(dirs):
+    answer = 0
+    U = [0,1]
+    D = [0, -1]
+    R = [1, 0]
+    L = [-1, 0]
+    
+    maps = [[0,0]]
+    
+    for i in range(len(dirs)) :
+        answer += 1
+        now = []
+        if dirs[i] == "U":
+            now = [maps[-1][0] + U[0], maps[-1][1] + U[1]]
+        elif dirs[i] == "D":
+            now = [maps[-1][0] + D[0], maps[-1][1] + D[1]]
+        elif dirs[i] == "R":
+            now = [maps[-1][0] + R[0], maps[-1][1] + R[1]]
+        elif dirs[i] == "L":
+            now = [maps[-1][0] + L[0], maps[-1][1] + L[1]]
+        
+        
+        if now[0] > 5 or now[0] < -5 or now[1] > 5 or now[1] < -5 :
+            answer -= 1
+            pass
+        else :
+            if now in maps:
+                now_index = []
+                for k in range(len(maps)) :
+                    if maps[k] == now :
+                        now_index.append(k)
+                for j in range(len(now_index)):
+                    if now_index[j] >= 1 and maps[now_index[j]-1] == maps[-1] :
+                        print(now)
+                        answer -= 1
+                        break
+                
+                    elif maps[now_index[j]+1] == maps[-1] :
+                        print(now)
+                        answer -= 1
+                        break
+                    
+            maps.append(now)
+    return answer
