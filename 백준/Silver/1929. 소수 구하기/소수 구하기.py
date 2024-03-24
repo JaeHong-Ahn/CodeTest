c49000191543ca5d90e29533e2323d0c0cb59bed
안재홍 <63873742+JaeHong-Ahn@users.noreply.github.com>
2024-03-24 22:19:01 +0900

    [Silver III] Title: 소수 구하기, Time: 168 ms, Memory: 38792 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,13 +1,17 @@
-#소수 구하기
-
 M, N = map(int, input().split())
 
-for i in range(M, N+1) :
-    if i == 1 :
-        continue
-    half = pow(i, 1/2)
-    for j in range(2, int(half)+1) :
-        if i % j == 0 :
-            break
-    else :
-        print(i)
+def eratos(M, N):
+
+    prime = [True] * (N + 1)
+    prime[0] = prime[1] = False
+
+    for i in range(2, int(N**(1/2)+1)):
+        if prime[i]:
+            for j in range(i*2, N+1, i):
+                prime[j] = False
+
+    for k in range(M, N+1):
+        if prime[k]:
+            print(k)
+
+eratos(M, N)
