9878e94b08bcbfc0bbd33312a12282d51447a305
안재홍 <63873742+JaeHong-Ahn@users.noreply.github.com>
2025-05-06 00:17:28 +0900

    [level 2] Title: 롤케이크 자르기, Time: 588.18 ms, Memory: 49.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,15 +1,16 @@
+from collections import Counter
+
 def solution(topping):
-    right_num, left_num = [], []
-    right_set, left_set = set(), set()
-
-    for idx, top in enumerate(topping):
-        right_set.add(top)
-        right_num.append(len(right_set))
-
-        left_set.add(topping[len(topping) - idx - 1])
-        left_num.append(len(left_set))
-
-    len_left = len(left_num)
-    answer = [i for i in range(len(right_num) - 1) if right_num[i] == left_num[len_left - i - 2]]
-
-    return len(answer)
+    answer = 0
+    cntr = Counter(topping)
+    cntr_set = set()
+    
+    for i in topping:
+        cntr[i] -= 1
+        cntr_set.add(i)
+        if cntr[i] == 0:
+            cntr.pop(i)
+        if len(cntr) == len(cntr_set):
+            answer += 1
+    
+    return answer
